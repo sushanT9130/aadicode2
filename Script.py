6904383d1cd46c7654fbe53d48af048e0ebce2e7
Sarthak Kale <83166658+sarthakkale16@users.noreply.github.com>
2022-10-07 00:37:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,27 +1,32 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
-
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+    START_TXT = """ Hey👋🏻 {} I Am ᴠᴀᴀʀɪ ! This Is Official Bot Of #ʀᴇǫᴜᴇsᴛʙᴏx1 ! You Must Buy Premium Membership At @rb1official\n
+ ✰ I Am A Advanced AutoFilter Bot Made with Python By #ʀᴇǫᴜᴇsᴛʙᴏx1 Enjoy By Adding To Your Group """
+    HELP_TXT = """Hey {} You Must Buy Premium Membership! Rates At @rb1official
+𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂"""
+    ABOUT_TXT = """
+✩ C𝚁𝙴𝙰𝚃𝙾𝚁 : <a href=https://t.me/know_sarthak16>✮ ꜱᴀʀᴛʜᴀᴋ ✮</a>
+✧ L𝙸B𝚁𝙰𝚁Y : ᵁⁿᵏⁿᵒʷⁿ
+✩ L𝙰N𝙶𝚄A𝙶E : 𝙿Y𝚃𝙷Ø𝙽 3.4+
+✧ D𝙰𝚃AB𝙰S𝙴 : Private Operated
+✩ H𝙾S𝚃S𝙴R𝚅𝙴R : Railway
+✧ 𝗕𝘂𝘆 𝗣𝗿𝗲𝗺𝗶𝘂𝗺 : <a href=https://t.me/rb1official>ᴛᴀᴘ ᴛᴏ ʙᴜʏ</a>
+✰ Ｏｗｎｅｒ １ : <a href=https://t.me/sarthakkale16>✪ ꜱᴀʀᴛʜᴀᴋ ✪</a>
+"""
+    SOURCE_TXT = """
+☛   <a href=https://t.me/requestbox1>❤️ 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹 𝗚𝗿𝗼𝘂𝗽 ❤️</a>\n
+☛   <a href=https://t.me/know_sarthak16>✌️ ꜱᴀʀᴛʜᴀᴋ ✌️</a>\n
+🔥 ɢɛռɛʀǟʟ ɨռʄօʀʍǟȶɨօռ 🔥\n
+I Am Not Creator Of This Source Code 😌 ! This Is Modified Version By ʀᴇǫᴜᴇsᴛʙᴏx1 ! I Cant Provide Source Code As It Is Private Now  \n
+<b>DEVS:</b>\n
+☞   <a href=https://t.me/helpsarthak_bot> 🥀 Ｄｅｖ Ｏｗｎｅｒ １ 🥀 </a> \n
+☞   <a href=https://t.me/deaddone> 💸 Ｄｅｖ Ｏｗｎｅｒ ２ 💸 </a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particularkeyword and
+  meenu will respond whenever a keywordfound the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. This Bot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -30,17 +35,25 @@ class script(object):
 • /filters - <code>list all the filters of a chat</code>
 • /del - <code>delete a specific filter in chat</code>
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
+
+    DIMR = """
+⚠️𝘿𝙄𝙎𝘾𝙇𝘼𝙄𝙈𝙀𝙍
+𝙰𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚗𝚝𝚎𝚗𝚝𝚜 𝚑𝚎𝚛𝚎 𝚊𝚛𝚎 𝚎𝚒𝚝𝚑𝚎𝚛 𝚏𝚘𝚛𝚠𝚊𝚛𝚍𝚎𝚍 𝚏𝚛𝚘𝚖 𝚘𝚝𝚑𝚎𝚛 𝚌𝚑𝚊𝚗𝚗𝚎𝚕 𝚘𝚛 𝚌𝚘𝚙𝚒𝚎𝚍 𝚏𝚛𝚘𝚖 𝚝𝚑𝚎𝚖. 𝚆𝚎 𝚍𝚘𝚗'𝚝 𝚘𝚠𝚗 𝚊𝚗𝚢 𝚘𝚏 𝚝𝚑𝚎 𝙼𝚘𝚟𝚒𝚎𝚜 𝚘𝚛 𝚂𝚎𝚛𝚒𝚎𝚜.
+𝙸𝚏 𝚈𝚘𝚞 𝚊𝚛𝚎 𝚝𝚑𝚎 𝚛𝚒𝚐𝚑𝚝𝚜 𝚘𝚠𝚗𝚎𝚛 𝚊𝚗𝚍 𝚠𝚊𝚗𝚝 𝚝𝚘 𝚛𝚎𝚖𝚘𝚟𝚎 𝚊𝚗𝚢 𝚌𝚎𝚛𝚝𝚊𝚒𝚗 𝚏𝚒𝚕𝚎𝚜, 𝙿𝚕𝚎𝚊𝚜𝚎 𝚛𝚎𝚙𝚘𝚛𝚝 𝚞𝚜, 𝚠𝚎 𝚊𝚛𝚎 𝚛𝚎𝚊𝚍𝚢 𝚝𝚘 𝚛𝚎𝚖𝚘𝚟𝚎 𝚝𝚑𝚘𝚜𝚎 𝚌𝚘𝚗𝚝𝚎𝚗𝚝 𝚊𝚜 𝚜𝚘𝚘𝚗 𝚊𝚜 𝚠𝚎 𝚌𝚊𝚗!
+𝙰𝚍𝚖𝚒𝚗 𝙸𝚜 𝙽𝚘𝚝 𝚁𝚎𝚜𝚙𝚘𝚗𝚜𝚒𝚋𝚕𝚎 𝙵𝚘𝚛 𝚊𝚗𝚢 𝙳𝚒𝚛𝚎𝚌𝚝 & 𝚒𝚗𝚍𝚒𝚛𝚎𝚌𝚝 𝙿𝚛𝚘𝚏𝚒𝚝 𝚕𝚘𝚜𝚜"""
+
+
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- This Bot Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. Varia supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/helpsarthak_bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
@@ -58,7 +71,7 @@ class script(object):
 
 <b>NOTE:</b>
 1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+2. Send <code>/connect</code> for connecting me to ur PM And Provide Files
 
 <b>Commands and Usage:</b>
 • /connect  - <code>connect a particular chat to your PM</code>
@@ -67,8 +80,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
-
+these are the extra features of ##ʀᴇǫᴜᴇsᴛʙᴏx1
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specified user.</code>
 • /info  - <code>get information about a user.</code>
@@ -77,7 +89,7 @@ these are the extra features of Eva Maria
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
 <b>NOTE:</b>
-This module only works for my admins
+Do Not Try To Be Oversmart This Is Admin Panel Back Of Now
 
 <b>Commands and Usage:</b>
 • /logs - <code>to get the rescent errors</code>
@@ -91,11 +103,13 @@ This module only works for my admins
 • /unban  - <code>to unban a user.</code>
 • /channel - <code>to get list of total connected channels</code>
 • /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+    STATUS_TXT = """╭──
+│<b>└Cᴏᴜɴᴛ ᴏғ Fɪʟᴇs</b>: <code>{}</code>
+│<b>└TᴏTᴀʟ Usᴇʀs</b>: <code>{}</code>
+│<b>└TᴏTᴀʟ Gʀᴏᴜᴘs</b>: <code>{}</code>
+│<b>└Usᴇᴅ Sᴛᴏʀᴀɢᴇ</b>: <code>{}</code>
+│<b>└Aᴠᴀɪʟᴀʙʟᴇ Sᴘᴀᴄᴇ</b>: <code>{}</code>
+╰────"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
