a7363eb945538c540b685eb40f7160f22ed6306f
Sarthak Kale <83166658+sarthakkale16@users.noreply.github.com>
2022-10-07 01:54:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/aadicode2.git /EvaMaria
+  git clone https://github.com/sarthakkale16/aadicode2.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /aadicode2
